5674868ab38729b13e7e9a77eb6642ca7cd21908
heejin.park <zzqq5414@gmail.com>
2025-05-16 23:24:37 +0900

    apply tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// import './App.css' // 주석 처리 또는 삭제
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
